8111fad40d4cd1906083f21f7720ea321a5e98e8
Yunitrish <yunthomas006@gmail.com>
2023-09-09 00:17:13 +0800

    init

---
+++ android/app/src/main/kotlin/com/example/ncue_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ncue_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'src/app.dart';
+import 'src/settings/settings_controller.dart';
+import 'src/settings/settings_service.dart';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  final settingsController = SettingsController(SettingsService());
+  await settingsController.loadSettings();
+  runApp(MyApp(settingsController: settingsController));
+}
