97d519cf9841d801818a63f962e0998fcf454008
Yunitrish <yunthomas006@gmail.com>
2023-09-13 07:56:09 +0800

    使用者資料系統完成

--- lib/src/features/basic/profile_view.dart
+++ lib/src/features/basic/profile_view.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ncue_app/src/features/basic/icon_route.dart';
 import 'package:ncue_app/src/features/basic/sign_in_view.dart';
 import 'package:ncue_app/src/features/settings/settings_view.dart';
+import 'package:ncue_app/src/features/user/user_model.dart';
+import 'package:ncue_app/src/features/user/user_service.dart';
 
 class ProfileView extends StatefulWidget {
   const ProfileView({super.key});
@@ -14,6 +17,26 @@ class ProfileView extends StatefulWidget {
 }
 
 class _ProfileViewState extends State<ProfileView> {
+  User? user = FirebaseAuth.instance.currentUser;
+
+  String name = "";
+  String type = "";
+  String uuid = "";
+
+  void load() async {
+    UserModel model = await UserService().loadUserData();
+    name = model.name;
+    type = model.type;
+    uuid = model.uuid;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    load();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return ProfileScreen(
@@ -25,13 +48,16 @@ class _ProfileViewState extends State<ProfileView> {
           Navigator.pushReplacementNamed(context, SignInView.routeName);
         })
       ],
-      children: const [
-        Align(
+      children: [
+        const Align(
           alignment: Alignment.centerRight,
           child: IconRoute(
               routeName: SettingsView.routeName,
               iconData: SettingsView.routeIcon),
-        )
+        ),
+        Text("name: $name"),
+        Text("uuid: $uuid"),
+        Text("type: $type"),
       ],
     );
   }
