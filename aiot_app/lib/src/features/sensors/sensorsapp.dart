cdabef0f1c3bcb1111c86daffcc509f13b4bab32
yuchi <kirklan0423@gmail.com>
2023-09-18 20:28:39 +0800

    Sensors

--- aiot_app/lib/src/features/sensors/sensorsapp.dart
+++ aiot_app/lib/src/features/sensors/sensorsapp.dart
@@ -1,5 +1,3 @@
-import 'dart:typed_data';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../basic/route_view.dart';
 import 'package:lottie/lottie.dart';
