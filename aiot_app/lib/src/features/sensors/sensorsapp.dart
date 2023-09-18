320b117712859bb95d5def013973f7cf753ab8bc
yuchi <kirklan0423@gmail.com>
2023-09-18 20:20:14 +0800

    Sensors

--- aiot_app/lib/src/features/sensors/sensorsapp.dart
+++ aiot_app/lib/src/features/sensors/sensorsapp.dart
@@ -20,10 +20,79 @@ class SensorsPageState extends State<SensorsPage> {
         title: const Text('Sensors Page'),
       ),
       body: Center(
-        child: Column(
+        child: Column(children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-
-            ]),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                height: 150,
+                width: 150,
+                child: Center(
+                    child: Container(
+                        child: Column(
+                  children: <Widget>[
+                    Lottie.asset(
+                      'assets/lottie/LightRain.json',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    ),
+                    Text("62.00%",
+                        style: TextStyle(
+                          fontSize: 25,
+                        )),
+                    Text('humidity')
+                  ],
+                ))),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue.withOpacity(0.2),
+                      spreadRadius: 6,
+                      blurRadius: 0,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 20),
+                height: 150,
+                width: 150,
+                child: Center(
+                    child: Container(
+                        child: Column(
+                  children: <Widget>[
+                    Lottie.asset(
+                      'assets/lottie/day.json',
+                      width: 100,
+                      height: 100,
+                      fit: BoxFit.fill,
+                    ),
+                    Text("32.50°C",
+                        style: TextStyle(
+                          fontSize: 25,
+                        )),
+                    Text('Temperature')
+                  ],
+                ))),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blue.withOpacity(0.2),
+                      spreadRadius: 6,
+                      blurRadius: 0,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ]),
       ),
     );
   }
