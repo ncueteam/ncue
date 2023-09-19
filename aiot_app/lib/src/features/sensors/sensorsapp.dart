8714fd5789e7402a3c8d852a5bf4a7f412ebfd10
yuchi <kirklan0423@gmail.com>
2023-09-20 00:07:44 +0800

    Sonsors

--- aiot_app/lib/src/features/sensors/sensorsapp.dart
+++ aiot_app/lib/src/features/sensors/sensorsapp.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../basic/route_view.dart';
 import 'package:lottie/lottie.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
 
 class SensorsPage extends RouteView {
   const SensorsPage({super.key})
@@ -11,6 +13,86 @@ class SensorsPage extends RouteView {
 }
 
 class SensorsPageState extends State<SensorsPage> {
+  late MqttServerClient client;
+  String messageText = "initial";
+
+  Widget messageComponent(String msg) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+        child: Text(msg),
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    connect();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  void connect() async {
+    client = MqttServerClient('test.mosquitto.org', 'ncue_app');
+    client.disconnect();
+    client.port = 1883;
+    client.logging(on: true);
+
+    client.onConnected = onConnected;
+    client.onSubscribed = onSubscribed;
+    client.onDisconnected = onDisconnected;
+    client.onUnsubscribed = onUnsubscribed;
+    client.pongCallback = pong;
+
+    final connMessage = MqttConnectMessage()
+        // .authenticateAs('username', 'password')
+        .withWillTopic('NCUEMQTT')
+        .withWillMessage('MQTT Connect from App')
+        .startClean()
+        .withWillQos(MqttQos.atLeastOnce);
+
+    client.connectionMessage = connMessage;
+
+    try {
+      await client.connect();
+    } catch (e) {
+      debugPrint('Exception: $e');
+      client.disconnect();
+    }
+  }
+
+  void onConnected() {
+    debugPrint('Connected');
+    client.subscribe('receive_topic', MqttQos.exactlyOnce);
+    client.subscribe('NCUEMQTT', MqttQos.exactlyOnce);
+    client.updates?.listen((List<MqttReceivedMessage<MqttMessage>> messages) {
+      for (var message in messages) {
+        final MqttPublishMessage payload =
+            message.payload as MqttPublishMessage;
+        messageText = MqttPublishPayload.bytesToStringAsString(payload.payload.message);
+      }
+    });
+  }
+
+  void onSubscribed(String topic) {}
+
+  void onDisconnected() {}
+
+  void onUnsubscribed(String? topic) {}
+
+  void pong() {}
+
+  void sendMessage(String topic, String message) {
+    final builder = MqttClientPayloadBuilder();
+    builder.addString(message);
+    client.publishMessage(topic, MqttQos.exactlyOnce, builder.payload!);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,11 +128,11 @@ class SensorsPageState extends State<SensorsPage> {
                       height: 100,
                       fit: BoxFit.fill,
                     ),
-                    const Text("62.00%",
+                    Text("62.00%",
                         style: TextStyle(
                           fontSize: 25,
                         )),
-                    const Text('humidity')
+                    Text('humidity')
                   ],
                 )),
               ),
@@ -78,11 +160,11 @@ class SensorsPageState extends State<SensorsPage> {
                       height: 100,
                       fit: BoxFit.fill,
                     ),
-                    const Text("32.50°C",
+                    Text(messageText,
                         style: TextStyle(
                           fontSize: 25,
                         )),
-                    const Text('Temperature')
+                    Text('Temperature')
                   ],
                 )),
               ),
