bb7129c116802c23f0ea078ef412595d56b04d02
Yunitrish <yunthomas006@gmail.com>
2023-10-02 16:35:36 +0800

    update

--- reset.bat
+++ reset.bat
@@ -1,2 +1,2 @@
-esptool -p com9 erase_flash
-esptool --chip esp32 -p COM9 write_flash -z 0x1000 esp32_ota_1.20.0.bin
+esptool -p %1 erase_flash
+esptool --chip esp32 -p %1 write_flash -z 0x1000 esp32_ota_1.20.0.bin
