71cd83c1bdcda21ef9d9939815455437dc94565f
Yunitrish <yunthomas006@gmail.com>
2024-01-04 22:49:24 +0800

    dht11 穩定度更新 採用ticks_diff 判定 避免因為while loop 直接運算造成的不準確

--- hardware/dht11/__init__.py
+++ hardware/dht11/__init__.py
@@ -1,29 +1,35 @@
 import dht
 import machine
+import time
 MAX_CYCLE = 32
 DEFAULT_PIN = 14
 
-class Sensor():
-    def __init__(self, pin = DEFAULT_PIN,mode="console") -> None:
+class dht11():
+    def __init__(self, pin = DEFAULT_PIN,mode="console",timer=machine.Timer(3)) -> None:
         import oled
         self.screen = oled.OLED(mode=mode)
         self.screen.display(["setting up","dht"])
-        self.cycle = 0
-        self.sensor = dht.DHT11(machine.Pin(pin))
+        self.dht11 = dht.DHT11(machine.Pin(pin))
+        
+        self.timer = timer
+        self.last_ticks = 0
+        
         self.hum = -1
         self.temp = -1
         
     def routine(self):
-        self.cycle += 1
-        try:
-            if (self.cycle > MAX_CYCLE):
-                self.screen.display(["getting","dht data"])
-                self.sensor.measure()
-                self.hum = self.sensor.humidity()
-                self.temp = self.sensor.temperature()
-                self.cycle = 0
-        except Exception as e:
-            print(str(e))
+        now = time.ticks_ms()
+        if time.ticks_diff(now,self.last_ticks) > 120:
+            self.last_ticks = now
+            try:
+#                 self.screen.display(["getting","dht data"])
+                self.dht11.measure()
+                self.hum = self.dht11.humidity()
+                self.temp = self.dht11.temperature()
+            except Exception as e:
+#                 print(str(e))
+                return
+        else:
             pass
 
     def getJson(self):
